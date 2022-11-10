0b7471c9c381a13de0a546fedee3f0017cbd70e8
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2022-11-10 09:15:18 +0600

    Initial Commit

---
+++ DrawingShapesAndPaths/DrawingShapesAndPathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingShapesAndPathsApp.swift
+//  DrawingShapesAndPaths
+//
+//  Created by rehnuma.deepty on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingShapesAndPathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
